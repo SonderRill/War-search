fbdb72fed1898aeb58d8631c87d327be854f115a
SonderRill <killer.kir@inbox.ru>
2019-10-31 15:38:41 +0600

    debag

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router()
-const fetch = require('node-fetch')
 const rp = require('request-promise')
-const axios = require('axios')
 
 router.get('/', (req, res) => {
   
